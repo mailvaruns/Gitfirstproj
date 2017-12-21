729c450fcc6fd4dabba2d06840e489c0758ff6ca
mailvaruns <varunsinghiit@gmail.com>
2017-12-21 12:45:45 +0530

    New file added

---
+++ Demo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package DemoProject1;
+
+/**
+ * @author e5548205
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Demo prject");
+
+	}
+
+}
